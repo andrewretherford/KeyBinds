e2624b0979dec9befcc51853923a948a1ca777b7
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2023-03-14 14:17:47 -0700

    adds warp command

--- keybinds.lua
+++ keybinds.lua
@@ -10,6 +10,8 @@ require('logger')
 -- Keybinds
 windower.register_event('load', function()
    windower.send_command('bind ~1 kb mount')
+   windower.send_command('bind ~2 kb trusts')
+   windower.send_command('bind ~3 kb warp')
 end)
 
 windower.register_event('addon command', function(command,...)
@@ -30,5 +32,19 @@ windower.register_event('addon command', function(command,...)
       else   
          windower.send_command('input /mount raptor')
       end
+
+   elseif command == 'warp' then
+      local equipment = windower.ffxi.get_items('equipment')
+      local bag = equipment['right_ring_bag']
+      local index = equipment['right_ring']
+      local item_data = windower.ffxi.get_items(bag, index)
+
+      if item_data.id == 28540 then
+         windower.send_command('input /item "warp ring" <me>')
+      else
+         windower.send_command('input /equip ring2 "warp ring"; wait 10; input /item "warp ring" <me>')
+      end   
    end
+
+   
 end)
