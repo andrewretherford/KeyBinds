56cd08e77e1c25b8e37f239cd5b50eca2dacd2ea
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2023-03-14 12:16:45 -0700

    adds logic for mount keybind

--- keybinds.lua
+++ keybinds.lua
@@ -7,8 +7,6 @@ _addon.language = 'english'
 -- Windower Libraries
 require('logger')
 
--- Globals
-
 -- Keybinds
 windower.register_event('load', function()
    windower.send_command('bind ~1 kb mount')
@@ -16,9 +14,21 @@ end)
 
 windower.register_event('addon command', function(command,...)
    command = command and command:lower()
+
    if command == 'mount' then
-      if windower.ffxi
-      windower.send_command('input /mount raptor')
-      notice("Mounting Raptor")
+      local player = windower.ffxi.get_player()
+      local mounted = false
+
+      for _, buff in pairs(player.buffs) do
+         if buff == 252 then
+            mounted = true
+         end
+      end
+
+      if mounted then
+         windower.send_command('input /dismount')
+      else   
+         windower.send_command('input /mount raptor')
+      end
    end
 end)
