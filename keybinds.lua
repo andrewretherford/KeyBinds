e9d4573dd94a3458c069772c5cad5f45298c737f
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2023-03-15 14:07:23 -0700

    adds add_trust function

--- keybinds.lua
+++ keybinds.lua
@@ -31,46 +31,48 @@ windower.register_event('load', function()
 end)
 
 windower.register_event('addon command', function(command, ...)
-   local arg = (...) and (...):lower()
    command = command and command:lower()
+   local args = table.concat({...}, ' ')
+   local arg_table = {}
+   
+   for argument in args:gmatch("%S+") do
+      table.insert(arg_table, argument)
+   end
 
    if command == 'mount' then
-      local player = windower.ffxi.get_player()
-      local mounted = false
-
-      for _, buff in pairs(player.buffs) do
-         if buff == 252 then
-            mounted = true
-         end
-      end
-
-      if mounted then
-         windower.send_command('input /dismount')
-      else   
-         windower.send_command('input /mount raptor')
-      end
+      mount()
 
    elseif command == 'showtr' then
       show_trusts()
 
    elseif command == 'addtr' then
-      add_trust(args[1], args[2])
+      add_trust(unpack(arg_table))
 
    elseif command == 'warp' then
-      local equipment = windower.ffxi.get_items('equipment')
-      local bag = equipment['right_ring_bag']
-      local index = equipment['right_ring']
-      local item_data = windower.ffxi.get_items(bag, index)
-
-      if item_data.id == 28540 then
-         windower.send_command('input /item "warp ring" <me>')
-      else
-         windower.send_command('input /equip ring2 "warp ring"; wait 10; input /item "warp ring" <me>')
-      end   
+      warp()
    end
 
    
 end)
+
+-- Mount Function
+
+function mount()
+   local player = windower.ffxi.get_player()
+   local mounted = false
+
+   for _, buff in pairs(player.buffs) do
+      if buff == 252 then
+         mounted = true
+      end
+   end
+
+   if mounted then
+      windower.send_command('input /dismount')
+   else   
+      windower.send_command('input /mount raptor')
+   end
+end
 
 -- Trust Functions
 
@@ -83,6 +85,46 @@ function show_trusts()
 end
 
 function add_trust(name, slot)
-   settings.trusts['trust'..slot] = name
+   local key
+   local key_name
+
+   if slot == '1' then
+      key = 't1'
+      key_name = 'Trust 1'
+   elseif slot == '2' then
+      key = 't2'
+      key_name = 'Trust 2'
+   elseif slot == '3' then
+      key = 't3'
+      key_name = 'Trust 3'
+   elseif slot == '4' then
+      key = 't4'
+      key_name = 'Trust 4'
+   elseif slot == '5' then
+      key = 't5'
+      key_name = 'Trust 5'
+   else
+      log('Enter a valid slot number (1-5)')
+      return
+   end
+
+   settings[key] = name
    settings:save()
+
+   log(key_name..': '..name)
+end
+
+-- Warp Function
+
+function warp()
+   local equipment = windower.ffxi.get_items('equipment')
+   local bag = equipment['right_ring_bag']
+   local index = equipment['right_ring']
+   local item_data = windower.ffxi.get_items(bag, index)
+
+   if item_data.id == 28540 then
+      windower.send_command('input /item "warp ring" <me>')
+   else
+      windower.send_command('input /equip ring2 "warp ring"; wait 10; input /item "warp ring" <me>')
+   end   
 end
