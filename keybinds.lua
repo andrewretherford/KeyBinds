8b618d85ffb3ef71c2e41dadaec2009d45e738ad
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2023-03-25 06:39:18 -0700

    adds toggle for multiboxing or solo play

--- keybinds.lua
+++ keybinds.lua
@@ -13,14 +13,17 @@ require('logger')
 require('tables')
 require('strings')
 
+multibox = true
 ----------------------------------
 -- Keybinds
 ----------------------------------
 
 windower.register_event('load', function()
-   windower.send_command('bind ~numpad7 send @all kb mount')
-   windower.send_command('bind ~numpad9 tm summontrusts')
-   windower.send_command('bind ~numpad3 kb warp')
+   windower.send_command("bind ~numpad7 send @all kb mount")
+   windower.send_command("bind ~numpad9 tm summontrusts")
+   windower.send_command("bind ~numpad3 kb warp")
+   windower.send_command("bind delete send skookum /ma 'cure' picklepants")
+   windower.send_command("bind home send skookum /attack <t>")
 end)
 
 ----------------------------------
@@ -36,9 +39,49 @@ windower.register_event('addon command', function(command, ...)
 
    elseif cmd == 'warp' then
       warp()
+
+   elseif cmd == 'mb' or cmd == 'multibox' then
+      toggle_multibox(args)
    end
 
 end)
+
+----------------------------------
+-- Utility
+----------------------------------
+
+function toggle_multibox(toggle)
+   if toggle == 'on' then
+      multibox = true
+      log('Multiboxing mode enabled')
+   elseif toggle == 'off' then
+      multibox = false
+      log('Multiboxing mode disabled')
+   elseif toggle == '' then
+      multibox = not multibox
+      if multibox == true then
+         log('Multiboxing mode enabled')
+      else
+         log('Multiboxing mode disabled')
+      end
+   else
+      log('You must enter either "on" or "off" ')
+   end
+
+   if multibox == true then
+      -- Keybinds for Multibox Mode
+      windower.send_command("bind ~numpad7 send @all kb mount")
+      windower.send_command("bind ~numpad3 send @all kb warp")
+      windower.send_command("bind delete send skookum /ma 'cure' picklepants")
+      windower.send_command("bind home send skookum /attack <t>")
+   else
+      -- Keybinds for Solo Mode
+      windower.send_command("bind ~numpad7 kb mount")
+      windower.send_command("bind ~numpad3 kb warp")
+      windower.send_command("unbind delete")
+      windower.send_command("unbind home")
+   end
+end
 
 ----------------------------------
 -- Utility
@@ -51,9 +94,9 @@ function warp()
    local item_data = windower.ffxi.get_items(bag, index)
 
    if item_data.id == 28540 then
-      windower.send_command("send @all input /item 'warp ring' <me>")
+      windower.send_command("input /item 'warp ring' <me>")
    else
-      windower.send_command("send @all input /equip ring2 'warp ring'; wait 11; send @all input /item 'warp ring' <me>")
+      windower.send_command("input /equip ring2 'warp ring'; wait 11; input /item 'warp ring' <me>")
    end   
 end
 
