3190d0f6517fbc77cee2c94eeadb1ed47d9f3308
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2023-03-28 12:59:40 -0700

    adds helper function to parse key and action from args

--- helper_functions.lua
+++ helper_functions.lua
@@ -1,16 +1,34 @@
+function get_key_and_action(args)
+   if #args < 2 then return false end
+
+   args = T{args}
+   local key = ''
+   local action = ''
+
+   if table.find(modifiers, args[1]) then
+      key = args[1]..' '..args[2]
+      action = table.concat(table.slice(args, 3, #args), ' ')
+   else
+      key = args[1]
+      action = table.concat(table.slice(args, 2, #args), ' ')
+   end
+
+   return key, args
+end
+
 function validate_key(key)
-   if key == nil or key == '' then return false
+   if key == nil or key == '' then return false end
 
    key = key
       :gsub('~', '')
       :gsub('^', '')
       :gsub('!', '')
 
-   return table.find(keybinds, key:lower())
+   return table.find(keybinds, key)
 end
 
 function display_keybind_format(key)
-   if not key or key == '' then return end
+   if not key or key == '' then return false end
 
    return key
       :gsub('~', 'Shift ')
@@ -19,10 +37,12 @@ function display_keybind_format(key)
 end
 
 function save_keybind_format(key)
-   if not key or key == '' then return end
+   if not key or key == '' then return false end
 
    if not string.startswith(key, '~') and not string.startswith(key, '^') and not string.startswith(key, '!') then
       return key
-         :gsub('')
+         :gsub('shift ', '~')
+         :gsub('ctrl ', '^')
+         :gsub('alt ', '!')
    end
 end
