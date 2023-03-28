c743c7603a788cdc44ac63a811778680b26bb4d9
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2023-03-28 12:59:04 -0700

    adds modifiers table to data.lua

--- data.lua
+++ data.lua
@@ -1,2 +1,10 @@
 keybinds = T{'`', 'escape', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace', 'tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'enter', 'return', 'ctrl', 'lctrl', 'rctrl', 'a', 's', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', ';', '\'', 'shift', 'lshift', 'rshift', '\\',  'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'alt', 'lalt', 'ralt', 'space', 'capslock', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12', 'numlock', 'numpad/', 'numpad*', 'numpad-', 'numpad+', 'numpad.', 'numpadenter', 'numpad1', 'numpad2', 'numpad3', 'numpad4', 'numpad5', 'numpad6', 'numpad7', 'numpad8', 'numpad9', 'numpad0', 'kana', 'convert', 'noconvert', 'yen', 'kanji', 'sysrq', 'scrolllock', 'pause', 'insert', 'home', 'pageup', 'delete', 'end', 'pagedown', 'up', 'down', 'left', 'right', 'windows', 'lwindows', 'rwindows', 'apps', 'mail', 'mmselect', 'mmstop', 'mute', 'mycomputer', 'mmnext', 'mmnexttrack', 'mmplaypause', 'mmprevtrack', 'mmstop', 'mmvolup', 'mmvoldown', 'webback', 'webforward', 'webhome', 'webfav', 'webrefresh', 'websearch', 'webstop', 'power'
 }
+
+modifiers = T{
+   ['~'] = 'shift', 
+   ['^'] = 'ctrl', 
+   ['!'] = 'alt', 
+   ['@'] = 'win', 
+   ['#'] = 'apps'
+}
